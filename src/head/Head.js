eb3388236574ad119d5e01157d97ed90c4622bba
Ravi727570 <148647125+Ravi727570@users.noreply.github.com>
2025-08-26 11:15:49 +0530

    Add files via upload

---
+++ src/head/Head.js
@@ -0,0 +1,20 @@
+import react from React;
+
+const Head=()=>{
+    return(
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/store">Store</Link>
+            </li>
+          </ul>
+        </div>
+    );
+};
+export default Head;
