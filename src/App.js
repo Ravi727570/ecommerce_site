8bf666a633bf9c4d4541917717b5ed575b8d4d77
Ravi727570 <148647125+Ravi727570@users.noreply.github.com>
2025-09-07 21:11:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,43 +5,62 @@ import About from "./pages/About";
 import Store from "./pages/Store";
 import Home from "./pages/Home";
 import Cart from "./head/Cart";
-import AuthPage from "./pages/AuthPage"; // AuthPage import
+import AuthPage from "./pages/AuthPage";
+import ProfilePage from "./pages/ProfilePage";
+import ContactUs from "./pages/ContactUs";
+import ProductPage from "./pages/ProductPage";
+import LoginPage from "./pages/LoginPage";
 import { CartProvider } from "./head/CartContext";
 import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const [cartOpen, setCartOpen] = useState(false);
 
   return (
-  <AuthProvider>
-    <CartProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} /> 
-          <Route path="/auth/*" element={<AuthPage />}/>
-          <Route path="/auth/profile" element={<ProfilePage />} />
-          <Route
-            path="/*"
-            element={
-              <>
-                <Navbar setCartOpen={setCartOpen} />
-                <Cart show={cartOpen} handleClose={() => setCartOpen(false)} />
-                <Routes>
-                  <Route path="/" element={<Navigate to="/store" replace />} />
-                  <Route path="/home" element={<Home />} />
-                  <Route path="/store" element={<Store />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/login" element={<LoginPage />} />
-                    <Route path="/contact" element={<ContactUs/>}/>
-                    <Route path="/products/:id" element={<ProductPage />} />
-                </Routes>
-              </>
-            }
-          />
-        </Routes>
-      </Router>
-    </CartProvider>
-  </AuthProvider>
+    <AuthProvider>
+      <CartProvider>
+        <Router>
+          <Navbar setCartOpen={setCartOpen} />
+          <Cart show={cartOpen} handleClose={() => setCartOpen(false)} />
+
+          <Routes>
+            {/* Default redirect */}
+            <Route path="/" element={<Navigate to="/store" replace />} />
+            
+
+            {/* Public routes */}
+            <Route path="/store" element={<Store />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/auth/*" element={<AuthPage />} />
+
+            {/* Protected routes */}
+            <Route
+              path="/products/:id"
+              element={
+                <ProtectedRoute>
+                  <ProductPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/auth/profile"
+              element={
+                <ProtectedRoute>
+                  <ProfilePage />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* Catch-all */}
+            <Route path="*" element={<Navigate to="/store" replace />} />
+          </Routes>
+        </Router>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
